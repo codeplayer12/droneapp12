b89f4227593ed13372caff2aa212ee4f7b0af9c9
codeplayer12 <noblemutoko@gmail.com>
2022-05-30 18:21:10 +0200

    staticfiles 2

--- drone/settings.py
+++ drone/settings.py
@@ -128,10 +128,12 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR , 'static')
 
 STATIC_DIRS = [
-    os.path.join(BASE_DIR,'staticfiles')
+    os.path.join(BASE_DIR,'static')
 ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
